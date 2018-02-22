c71f1ea58f12cf446525493b01ac1de6cbfffde4
AndreaCavagna <andrea.cavagna.94@gmail.com>
2018-02-22 14:51:58 +0100

    aaa

--- appmain.py
+++ appmain.py
@@ -1,19 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import requests
-import os
 
 
-# Create your views here.
-def  __init__() :
-    return "ciso"
 
-def  __main__() :
-    return "ciso"
 
-def index(request):
-    # return HttpResponse('Hello from Python!')
-
-    return render(request, '<h1>' + 'helloworld' + '</h1>')
-
-render( '<h1>' + 'helloworld' + '</h1>')
+print('<h1>' + 'helloworld' + '</h1>')
