9b2f6b2e6d6b150ebbf536a8977fc8fb918a362b
AndreaCavagna <andrea.cavagna.94@gmail.com>
2018-02-22 14:30:16 +0100

    aaa

--- appmain.py
+++ appmain.py
@@ -1,5 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
 import requests
 import os
 
+
+# Create your views here.
 def index(request):
-    return "Hello"
+    # return HttpResponse('Hello from Python!')
+
+    return render(request, '<h1>' + 'helloworld' + '</h1>')
